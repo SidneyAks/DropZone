ff227deb69c0d7fa8f901b75957ea8186fdd7408
SidneyAks <Sidneyaks@gmail.com>
2021-02-05 16:35:47 -0600

    Added Ability to set up for-a-given-monitor-only screens

--- ZoneRenderer/ZoneAbastract.cs
+++ ZoneRenderer/ZoneAbastract.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Serialization;
 
 namespace ZoneRenderer
@@ -11,6 +13,14 @@ namespace ZoneRenderer
 
         [XmlAttribute]
         public LayoutKind Layout { get; set; }
+
+        [XmlAttribute]
+        public string Screens { get; set; }
+
+        public int[] ScreenIndexes {
+            get => Screens.Split(',').Select(x => Int32.Parse(x)).ToArray();
+            set => Screens = string.Join(",", value);
+        }
      
         public Bounds<T> Target { get; set; }
         public Bounds<T> Trigger { get; set; }
